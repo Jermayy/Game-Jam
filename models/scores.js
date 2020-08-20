76c1e2dcde7bf6e376141f0f479968759cb790af
Mo Pishdar <mohpish@gmail.com>
2020-08-21 00:52:54 +1000

    ign review scores table was also added so now we have two tables to work with and can search for those games who also has a review score on ign

---
+++ models/scores.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, DataTypes) => {
+    const Score = sequelize.define("Score", {
+        game: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        platform: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        score: {
+            type: DataTypes.DECIMAL(4, 2)
+        },
+        genre: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        genre: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        }
+    }, {
+        timestamps: false
+    });
+    return Score;
+};
