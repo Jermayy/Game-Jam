4361e2b03f7377ad72fd0ae599badc4f1ad40469
Jeremy <jeremyVDLRC@gmail.com>
2020-08-27 22:01:15 +1000

    url for api query for images commented

--- public/assets/js/games.js
+++ public/assets/js/games.js
@@ -1,78 +1,69 @@
 $(document).ready(() => {
-  console.log("ready!");
+    console.log("ready!");
 
-  $("#submitBtn").on("click", event => {
-    event.preventDefault();
+    $("#submitBtn").on("click", event => {
+        event.preventDefault();
 
-    const gameNameValue = $("#gameSearch")
-      .val()
-      .trim();
+        const gameNameValue = $("#gameSearch")
+            .val()
+            .trim();
 
-    const genreValue = $("#genre").val();
-    console.log(genreValue);
+        const genreValue = $("#genre").val();
+        console.log(genreValue);
 
-    const platformValue = $("#platforms").val();
-    console.log(platformValue);
+        const platformValue = $("#platforms").val();
+        console.log(platformValue);
 
-    if (genreValue === "none" && platformValue === "none") {
-      $.get("/api/games/" + gameNameValue, data => {
-        console.log(data);
-      });
-    } else if (genreValue === "none") {
-      $.get(
-        "/api/gamesplatforms/" + gameNameValue + "/" + platformValue,
-        data => {
-          console.log(data);
+        if (genreValue === "none" && platformValue === "none") {
+            $.get("/api/games/" + gameNameValue, data => {
+                console.log(data);
+            });
+        } else if (genreValue === "none") {
+            $.get(
+                "/api/gamesplatforms/" + gameNameValue + "/" + platformValue,
+                data => {
+                    console.log(data);
+                }
+            );
+        } else if (platformValue === "none") {
+            $.get("/api/gamesgenre/" + gameNameValue + "/" + genreValue, data => {
+                console.log(data);
+            });
+        } else {
+            $.get(
+                "/api/gamesgenreplatform/" +
+                gameNameValue +
+                "/" +
+                genreValue +
+                "/" +
+                platformValue,
+                data => {
+                    console.log(data);
+                    console.log(data[0].game);
+                }
+            );
         }
-      );
-    } else if (platformValue === "none") {
-      $.get("/api/gamesgenre/" + gameNameValue + "/" + genreValue, data => {
-        console.log(data);
-      });
-    } else {
-      $.get(
-        "/api/gamesgenreplatform/" +
-          gameNameValue +
-          "/" +
-          genreValue +
-          "/" +
-          platformValue,
-        data => {
-          console.log(data);
-        }
-      );
-    }
-  });
+    });
 });
 
-// $.get("/api/posts" + categoryString, (data) => {
-//     console.log("Posts", data);
-//     posts = data;
-//     if (!posts || !posts.length) {
-//         displayEmpty();
-//     } else {
-//         initializeRows();
-//     }
-// });
 
-// $(".create-form").on("submit", (event) => {
-//     // Make sure to preventDefault on a submit event.
-//     event.preventDefault();
 
-//     const newCat = {
-//         name: $("#ca").val().trim(),
-//         sleepy: $("[name=sleepy]:checked").val().trim()
-//     };
 
-//     // Send the POST request.
-//     $.ajax("/api/cats", {
-//         type: "POST",
-//         data: newCat
-//     }).then(
-//         () => {
-//             console.log("created new cat");
-//             // Reload the page to get the updated list
-//             location.reload();
-//         }
-//     );
-// });
+
+// API query:
+const getImage = (gameQuery) => {
+    apiKey = "";
+    let queryURL = "https://www.gamespot.com/api/games/?api_key=" + apiKey + gameQuery;
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function(response) {
+        console.log(response);
+
+    });
+
+}
+
+// https://www.gamespot.com/api/games/?api_key=<apiKey>&filter=name:Doom&format=json
