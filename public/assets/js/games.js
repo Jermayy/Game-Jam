21129f587001162701441059904a5e4cd62e4c97
Mo Pishdar <mohpish@gmail.com>
2020-08-29 02:13:20 +1000

    search field and dropdown lists now  will retain their previous state when the original search was made so user can see what he did search for

--- public/assets/js/games.js
+++ public/assets/js/games.js
@@ -1,4 +1,55 @@
 $(document).ready(() => {
+  //retaining the input fileds value from the url so we have our search keywords and clauses back in place after the redirect
+  function populateInputFields() {
+    const urlAdress = window.location.href.split("/");
+    if (urlAdress[urlAdress.length - 2] === "games") {
+      //cleaning the values for any %20s added to them by the browser if there is any space in their name
+      const gameName = urlAdress[urlAdress.length - 1].split("%20").join(" ");
+      //assigning the captured game name value from the url as the default value for the search box
+      $(".formSearchInputBar").attr("value", gameName);
+    } else if (urlAdress[urlAdress.length - 3] === "gamesgenre") {
+      const gameName = urlAdress[urlAdress.length - 2].split("%20").join(" ");
+      $(".formSearchInputBar").attr("value", gameName);
+      //making the captured genre value as the default value for the dropdown list for genre. Same thing applies for dropdwon list of platforms
+      $(
+        "#genre option[value=" +
+          "'" +
+          urlAdress[urlAdress.length - 1] +
+          "'" +
+          "]"
+      ).attr("selected", "selected");
+    } else if (urlAdress[urlAdress.length - 3] === "gamesplatforms") {
+      const gameName = urlAdress[urlAdress.length - 2].split("%20").join(" ");
+      const plaformName = urlAdress[urlAdress.length - 1]
+        .split("%20")
+        .join(" ");
+      $(".formSearchInputBar").attr("value", gameName);
+      $("#platforms option[value=" + "'" + plaformName + "'" + +"]").attr(
+        "selected",
+        "selected"
+      );
+    } else if (urlAdress[urlAdress.length - 4] === "gamesgenreplatform") {
+      const gameName = urlAdress[urlAdress.length - 3].split("%20").join(" ");
+      const plaformName = urlAdress[urlAdress.length - 1]
+        .split("%20")
+        .join(" ");
+      $(".formSearchInputBar").attr("value", gameName);
+      $("#platforms option[value=" + "'" + plaformName + "'" + "]").attr(
+        "selected",
+        "selected"
+      );
+      $(
+        "#genre option[value=" +
+          "'" +
+          urlAdress[urlAdress.length - 2] +
+          "'" +
+          "]"
+      ).attr("selected", "selected");
+    }
+  }
+  //runing the function above on page load to ensure populating the input fields
+  window.onload = populateInputFields;
+
   $("#submitBtn").on("click", event => {
     event.preventDefault();
     //Getting the values out of the input fields and dropdown boxes and storing them inside const variables
@@ -27,9 +78,6 @@ $(document).ready(() => {
       window.location.replace(
         "http://localhost:8080/api/games/" + gameNameValue
       );
-      //retaining the input fileds value from the url so we have our search keywords and clauses back in place after the redirect
-      const urlAdress = window.location.href.split("/");
-      $(".formSearchInputBar").attr("value", urlAdress[urlAdress.length - 1]);
     } else if (genreValue === "none") {
       $.get(
         "/api/gamesplatforms/" + gameNameValue + "/" + platformValue,
